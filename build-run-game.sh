fa39d59a5c1f966f7f8951d616bca1f0b9886043
h4de5 <h4de5@users.noreply.github.com>
2018-02-26 02:03:07 +0100

    Add advise for port.

--- build-run-game.sh
+++ build-run-game.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" == 0 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
   echo "To run your godot game as a headless server, go to your source directory and run:"
-  echo "  docker run --rm -it -v \`pwd\`:/root/workspace/game/ h4de5/docker-godot-3-build-and-run:latest /root/workspace/build-scripts/${0} <path-to-server.tscn>"
+  echo "  docker run --rm -it -p <port:port> -v \`pwd\`:/root/workspace/game/ h4de5/docker-godot-3-build-and-run:latest /root/workspace/build-scripts/${0} <path-to-server.tscn>"
   exit 0
 fi
 
